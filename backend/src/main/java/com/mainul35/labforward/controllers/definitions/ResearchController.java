8444177b0c7551c35e1975224635e5686bc66673
mainul35 <mainuls18@gmail.com>
2021-10-27 02:37:40 +0700

    validations added

--- backend/src/main/java/com/mainul35/labforward/controllers/definitions/ResearchController.java
+++ backend/src/main/java/com/mainul35/labforward/controllers/definitions/ResearchController.java
@@ -5,17 +5,21 @@ import com.mainul35.labforward.controllers.dtos.request.ResearchDto;
 import com.mainul35.labforward.controllers.dtos.response.SimilarWordsResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
+@Validated
 @RequestMapping("/research")
 public interface ResearchController {
 
     @GetMapping(value = "/frequency", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<FrequencyResponse> getFrequencies(@RequestBody ResearchDto researchDto);
+    ResponseEntity<FrequencyResponse> getFrequencies(@Valid @RequestBody ResearchDto researchDto);
 
     @GetMapping(value = "/similar-words", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<SimilarWordsResponse> getSimilarWords(@RequestBody ResearchDto researchDto);
+    ResponseEntity<SimilarWordsResponse> getSimilarWords(@Valid @RequestBody ResearchDto researchDto);
 
 }
--- backend/src/main/java/com/mainul35/labforward/controllers/dtos/request/ResearchDto.java
+++ backend/src/main/java/com/mainul35/labforward/controllers/dtos/request/ResearchDto.java
@@ -2,10 +2,18 @@ package com.mainul35.labforward.controllers.dtos.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 public class ResearchDto implements Serializable {
+
+    @NotNull(message = "Given word must not be null")
+    @NotBlank(message = "Given word must not be empty")
     private String givenWord;
+
+    @NotNull(message = "Note book entry must not be null")
+    @NotBlank(message = "Note book entry must not be empty")
     private String notebookEntry;
 }
